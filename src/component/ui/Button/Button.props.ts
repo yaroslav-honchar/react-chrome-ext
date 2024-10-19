1d90adfd0004050aa4e17f22576ad7c4b83c6c5f
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-10-19 13:35:01 +0300

    feat: updated button & test components with props doc

--- src/component/ui/Button/Button.props.ts
+++ src/component/ui/Button/Button.props.ts
@@ -4,7 +4,23 @@ import type { buttonVariants } from "./Button.variants"
 type AnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement>
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 
+/**
+ * Props for the Button component.
+ *
+ * @extends {Partial<AnchorProps & ButtonProps>}
+ */
 export interface IButtonProps extends Partial<AnchorProps & ButtonProps> {
+  /**
+   * Variant of the button, corresponding to a key in buttonVariants.
+   *
+   * @type {keyof typeof buttonVariants}
+   */
   variant?: keyof typeof buttonVariants
+
+  /**
+   * HTML tag to be used for rendering the button.
+   *
+   * @type {"button" | "a"}
+   */
   tag?: "button" | "a"
 }
--- src/component/ui/Text/Text.props.ts
+++ src/component/ui/Text/Text.props.ts
@@ -1,8 +1,30 @@
 import type React from "react"
 import type { textVariants } from "./Text.variants"
 
+/**
+ * Props for the Text component.
+ *
+ * @extends {React.HTMLProps<HTMLDivElement>}
+ */
 export interface ITextProps extends React.HTMLProps<HTMLDivElement> {
+  /**
+   * Variant of the text, corresponding to a key in textVariants.
+   *
+   * @type {keyof typeof textVariants}
+   */
   variant?: keyof typeof textVariants
+
+  /**
+   * HTML tag to be used for rendering the text.
+   *
+   * @type {"p" | "span" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6"}
+   */
   tag?: "p" | "span" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+
+  /**
+   * Number of lines to clamp the text to.
+   *
+   * @type {number}
+   */
   lineClamp?: number
 }
